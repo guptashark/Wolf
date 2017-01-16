0d949f44477bacd807801dd6ba44a5351c4cbe86
Aishwary Gupta <guptashark@gmail.com>
2017-01-16 15:21:27 -0500

    created tester segment for bstree library

--- Makefile
+++ Makefile
@@ -3,6 +3,10 @@ CC = cl
 CFLAGS = /Wall /WX /wd4710
 SRC = main.c bstree.c
 
+bstree_test:
+	$(CC) $(CFLAGS) bstree.c bstree_tester.c
+
 all: 
 	$(CC) $(CFLAGS) $(SRC)
+
 
---
+++ bstree_tester.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <assert.h>
+#include "bstree.h"
+
+int bst_key_cmp(void *a, void *b) {
+	(void)a;
+	(void)b;
+	return 0;
+}
+
+int main(void) {
+
+	int result = 0;
+
+	/* test for proper arg checking in bst_init */
+	{
+		struct bstree *sample_bst = (struct bstree *)10;
+		struct bstree **bst_ptr = &sample_bst;
+		result = bstree_init(bst_ptr, bst_key_cmp);
+		assert(1 == result);
+	}
+
+	/* test for proper checking in bst_init key_cmp_fn */
+	result = 0;
+
+	{	
+		struct bstree *sample_bst = NULL;
+		struct bstree **bst_ptr = &sample_bst;
+		result = bstree_init(bst_ptr, NULL);
+		assert(2 == result);
+	}
+
+	printf("All tests passed.\n");
+	return 0;
+}
